d08db1eabc0b743a5c8c40ca11b34f3a0fe9e35a
Vuong Minh <vuongxuongminh@gmail.com>
2018-05-30 09:55:30 +0700

    Unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,7 @@ define('YII_DEBUG', true);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/compatibility.php');
 
 Yii::setAlias('@yiiviet/tests/unit/n2w', __DIR__);
 Yii::setAlias('@yiiviet/n2w', dirname(__DIR__) . '/src');
